fa0301543a1f7be00c0527b48eacf65a1e566c1e
Simon Plourde <simon@sensu.io>
2018-11-30 12:58:26 -0500

    Allow go run to be ran
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- cmd/sensu-agent/main.go
+++ cmd/sensu-agent/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/sensu/sensu-go/agent/cmd"
+import (
+	"github.com/sensu/sensu-go/agent/cmd"
+	"github.com/sirupsen/logrus"
+)
+
+var logger = logrus.WithFields(logrus.Fields{
+	"component": "agent",
+})
 
 func main() {
 	if err := cmd.Execute(); err != nil {
--- cmd/sensu-backend/main.go
+++ cmd/sensu-backend/main.go
@@ -5,7 +5,12 @@ import (
 
 	"github.com/sensu/sensu-go/backend"
 	"github.com/sensu/sensu-go/backend/cmd"
+	"github.com/sirupsen/logrus"
 )
+
+var logger = logrus.WithFields(logrus.Fields{
+	"component": "backend",
+})
 
 func main() {
 	if err := cmd.Execute(backend.Initialize); err != nil {
